7abb0bc4b42f17dd576fb0286ef4a2b0d52edecd
Danh <vothanhdanh9526@gmail.com>
2020-04-17 08:12:25 +0700

    dump question

---
+++ easy_DivisorGame.py
@@ -0,0 +1,13 @@
+class SelfImplement():
+    def divisorGame(self, N):
+        '''
+            1: Lose
+            2: Win
+            3: Lose
+            4: Win
+            5: Lose
+        '''
+
+        if N%2 == 0:
+            return True
+        return False
